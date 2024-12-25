00002ec49a2c35f7b14d5286fd16ba603859f618
FaresIbrahim32 <105607075+FaresIbrahim32@users.noreply.github.com>
2024-12-25 13:50:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -629,4 +629,4 @@ if __name__ == '__main__':
     with app.app_context():
         init_db()  # Initialize the database tables
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port,debug=True)
